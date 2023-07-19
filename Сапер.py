a21fbd2277ca91ee3e3f06fc773c3445d4e6e2a0
Igor <iiigorrr@yandex.ru>
2023-07-19 17:45:57 +0400

    чуть намудрил
    чуть намудрил

--- Сапер.py
+++ Сапер.py
@@ -1,7 +1,8 @@
 from random import randint
 
+
 class Cell:
-    def __init__(self,around_mines = 0,  mine = False):
+    def __init__(self, around_mines=0, mine=False):
         self.around_mines = around_mines
         self.mine = mine
         self.fl_open = False
@@ -14,25 +15,31 @@ class GamePole:
         self.pole = [[Cell() for i in range(self.N)] for j in range(self.N)]
         self.init()
 
-
-
-
     def init(self):
         m = 0
         while m <= self.M:
-            for i in self.pole in range(0, randint(0, self.M)):
-                print(type(self.pole))
-                for j in i in range(0, randint(0, self.M)):
-                    if self.pole.mine:
+            for i in range(0, randint(0, self.M-1)):
+                print(m, "это м1")
+                for j in range(0, randint(0, self.M-1)):
+                    print(m, "это м2")
+                    if self.pole[i][j].mine == True:
+                        print("was")
                         continue
                     else:
-                        self.pole.mine = True
-
-
-
+                        self.pole[i][j].mine = True
+                        print("put")
+                    m += 1
+                    print(m)
     def show(self):
-        pass
+        for i in self.pole:
+            print(list(map(self.oh(), i)))
 
-a = GamePole(5,5)
+    def oh(self):
+        if i == True:
+            return "*"
+        else:
+            return "#"
+
+a = GamePole(5, 5)
+a.show()
 print(a)
-
