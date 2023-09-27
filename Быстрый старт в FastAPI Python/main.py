683999c3351f1b0558840cd22136aeb5ef03ca4e
Igor <iiigorrr@yandex.ru>
2023-09-27 16:23:34 +0400

    27.09.2023 продвинулся по степику

--- Быстрый старт в FastAPI Python/main.py
+++ Быстрый старт в FastAPI Python/main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def read_root():
+    return {"message": "Hello, World!"}
+
+
+@app.get("/custom")
+def read_custom_message():
+    return {"message": "This is a custom message!"}
+
+
+
+@app.get("/html")
+def root():
+    with open("index.html", "r", encoding= "utf-8") as a:
+        b = a.read()
+
+    return HTMLResponse(content = b)
+
+@app.post("/calculate")
+def func(num1: int, num2: int):
+    return {"result": num1+num2}
