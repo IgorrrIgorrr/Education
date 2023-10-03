b32ca1405841c9404a2621ed89d49430bb203e69
Igor <iiigorrr@yandex.ru>
2023-10-03 17:41:11 +0400

    залез в дебри

--- Быстрый старт в FastAPI Python/App3/main.py
+++ Быстрый старт в FastAPI Python/App3/main.py
@@ -1,16 +1,28 @@
-from fastapi import FastAPI, Response, Depends
+from fastapi import FastAPI, Response, Depends, Cookie
 from fastapi import Form
+from pydantic import BaseModel
 from typing_extensions import Annotated
-import random, httpx
+import random
 
 app = FastAPI()
 
 fake_bd = {
     "Igor": {
         "name": "Igor",
-        "password": "abc123"
+        "password": "abc123",
+        "id": "1",
+    },
+    "Mike": {
+        "name":"Mike",
+        "password": "aaa",
+        "id":"2",
     }
 }
+
+
+class User(BaseModel):
+    name: str
+    password: str
 
 
 def authenticate(bd, username: str, password: str):
@@ -23,16 +35,17 @@ def gener_value():
     return str(random.random())
 
 
-@app.get("/login")
+@app.put("/login")
 def login(username: Annotated[str, Form()], password: Annotated[str, Form()], response: Response,
           unic: Annotated[str, Depends(gener_value)]):
     if authenticate(fake_bd, username, password):
         response.set_cookie(key="session_token", secure=True, value=unic)
         return {"a": "good"}
 
+@app.get("/user")
+def get_user(session_token = Cookie(), id: int = Cookie()):
+    if session_token == unic:
+        us_dict = fake_bd[us]
+        return {"session_token": session_token}
 
-url = "http://127.0.0.1:8000/login"
-params = {"username": "Igor", "password": "abc123"}
-response = httpx.get(url, params=params)
-print(response.status_code)
-print(response.json())
+
