a0f88da08febb12f8c17bfb21d91bcde7160198f
Igor <iiigorrr@yandex.ru>
2023-10-03 14:58:14 +0400

    Перед большим httpx

--- Быстрый старт в FastAPI Python/App3/main.py
+++ Быстрый старт в FastAPI Python/App3/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Response, Depends
 from fastapi import Form
 from typing_extensions import Annotated
-import random
+import random, httpx
 
 app = FastAPI()
 
@@ -28,3 +28,11 @@ def login(username: Annotated[str, Form()], password: Annotated[str, Form()], re
           unic: Annotated[str, Depends(gener_value)]):
     if authenticate(fake_bd, username, password):
         response.set_cookie(key="session_token", secure=True, value=unic)
+        return {"a": "good"}
+
+
+url = "http://127.0.0.1:8000/login"
+params = {"username": "Igor", "password": "abc123"}
+response = httpx.get(url, params=params)
+print(response.status_code)
+print(response.json())
