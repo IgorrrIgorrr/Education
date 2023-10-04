69dd1405f41d06f3f74409da320436a65861eba2
Igor <iiigorrr@yandex.ru>
2023-10-04 11:53:44 +0400

    finished path user

--- Быстрый старт в FastAPI Python/App3/main.py
+++ Быстрый старт в FastAPI Python/App3/main.py
@@ -10,12 +10,12 @@ fake_bd = {
     "Igor": {
         "name": "Igor",
         "password": "abc123",
-        "id": "1",
+        "session_token": None,
     },
     "Mike": {
         "name":"Mike",
         "password": "aaa",
-        "id":"2",
+        "session_token":None,
     }
 }
 
@@ -35,17 +35,24 @@ def gener_value():
     return str(random.random())
 
 
+username_form = Form()
+password_form = Form()
+
+
+
+
 @app.put("/login")
-def login(username: Annotated[str, Form()], password: Annotated[str, Form()], response: Response,
+def login(username: str = username_form, password: str = password_form, response: Response,
           unic: Annotated[str, Depends(gener_value)]):
     if authenticate(fake_bd, username, password):
         response.set_cookie(key="session_token", secure=True, value=unic)
+        fake_bd[username]["session_token"] = unic
         return {"a": "good"}
 
 @app.get("/user")
-def get_user(session_token = Cookie(), id: int = Cookie()):
-    if session_token == unic:
-        us_dict = fake_bd[us]
-        return {"session_token": session_token}
+def get_user(session_token = Cookie()):
+    if fake_bd[username_form]["session_token"] == session_token:
+        user = fake_bd[username_form]
+        return User(**user)
 
 
