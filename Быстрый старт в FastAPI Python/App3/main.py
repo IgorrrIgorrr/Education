eed6a9c43621b59353db13040e00654c695b3534
Igor <iiigorrr@yandex.ru>
2023-10-05 08:54:43 +0400

    Не корректно работает путь user не реагирует на изменение токена

--- Быстрый старт в FastAPI Python/App3/main.py
+++ Быстрый старт в FastAPI Python/App3/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Response, Depends, Cookie
-from fastapi import Form
+from fastapi import Form, HTTPException, status
 from pydantic import BaseModel
 from typing_extensions import Annotated
 import random
@@ -13,9 +13,9 @@ fake_bd = {
         "session_token": None,
     },
     "Mike": {
-        "name":"Mike",
+        "name": "Mike",
         "password": "aaa",
-        "session_token":None,
+        "session_token": None,
     }
 }
 
@@ -39,20 +39,22 @@ username_form = Form()
 password_form = Form()
 
 
-
-
 @app.put("/login")
-def login(username: str = username_form, password: str = password_form, response: Response,
-          unic: Annotated[str, Depends(gener_value)]):
-    if authenticate(fake_bd, username, password):
+def login(unic: Annotated[str, Depends(gener_value)], response: Response, username: str = username_form,
+          password: str = password_form):
+    if not authenticate(fake_bd, username, password):
+        raise HTTPException(status_code=404, detail="You are not registrated")
+    else:
         response.set_cookie(key="session_token", secure=True, value=unic)
         fake_bd[username]["session_token"] = unic
-        return {"a": "good"}
+        return {"Success": "good"}, {"your session_token": unic}
 
-@app.get("/user")
-def get_user(session_token = Cookie()):
-    if fake_bd[username_form]["session_token"] == session_token:
-        user = fake_bd[username_form]
-        return User(**user)
 
+@app.put("/user")
+def get_user(session_token=Cookie(), username: str = username_form):
+    if not fake_bd[username]["session_token"] == session_token:
+        raise HTTPException(status_code=401, detail={"message": "Unauthorized"})
+    else:
+        user = fake_bd[username]
+        return User(**user), fake_bd[username]["session_token"], session_token
 
---
+++ всякое/Запоминание кода/1.py
@@ -0,0 +1,3 @@
+from typing_extensions import Annotated
+
+from fastapi import Depends, FastAPI, HTTPException, OAuth
